ceaf975d10f8d994b40115a688c89574c916f8ed
karamalesa1980 <karamalesa@gmail.com>
2020-01-14 19:59:14 +0200

    add jquery.easydropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
+//require('admin-lte')
+
+// import jQuery from "jquery";
+window.jQuery = $;
+window.$ = $;
+
 
 
 
@@ -16,3 +23,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//import 'bootstrap-sass';
+import 'bootstrap/dist/js/bootstrap';
+import 'styles/application.scss';
+import 'packs/js/jquery.easydropdown.js';
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,45 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}));
+
+environment.loaders.append('jquery', {
+  test: require.resolve('jquery'),
+  use: [{
+    loader: 'expose-loader',
+    options: '$',
+  }, {
+    loader: 'expose-loader',
+    options: 'jQuery',
+  }],
+});
 module.exports = environment
+
+// config/webpack/environment.js
+// const { environment } = require('@rails/webpacker')
+// const webpack = require('webpack')
+// // Add an additional plugin of your choosing : ProvidePlugin
+// environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     JQuery: 'jquery',
+//     jquery: 'jquery',
+//     'window.Tether': "tether",
+//     Popper: ['popper.js', 'default'], // for Bootstrap 4
+//   })
+// )
+// module.exports = environment
+
+// const { environment } = require('@rails/webpacker')
+
+//   environment.plugins.prepend('Provide',
+//     new webpack.ProvidePlugin({
+//       $: 'jquery/src/jquery',
+//       jQuery: 'jquery/src/jquery'
+//     })
+//   )
+
+// module.exports = environment
