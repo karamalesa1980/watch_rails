0df3832e40c6fd3ee8ca1c36ddbfeef58cb48a5d
karamalesa1980 <karamalesa@gmail.com>
2020-02-20 13:39:53 +0200

    fix turbolinks with jquery step 3

--- app/javascript/packs/flexs.js
+++ app/javascript/packs/flexs.js
@@ -1,5 +1,5 @@
 // Can also be used with $(document).ready()
-$(window).on('load',function() {
+$(document).ready(function() {
     $('.flexslider').flexslider({
         animation: "slide",
         controlNav: "thumbnails"
