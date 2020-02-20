0cac1d23670911598a172fb112e393998f23a274
karamalesa1980 <karamalesa@gmail.com>
2020-02-20 13:40:18 +0200

    fix turbolinks with jquery step 4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,19 @@
 
-import "../styles/application.scss";
+
 //import 'bootstrap';
 //import 'packs/js/jquery.bxslider.js';
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+require("jquery");
+require("jquery.turbolinks");
+require("jquery_ujs");
 require("@rails/ujs").start();
-//require("turbolinks").start();
+require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("jquery");
-//require("jquery.turbolinks").start();
+
 
 //require("flexslider")
 //require('admin-lte')
@@ -22,10 +23,10 @@ require("jquery");
 // window.$ = $;
 //import './js/jquery-1.11.0.min.js';
 
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
-});
+// document.addEventListener("turbolinks:load", () => {
+//   $('[data-toggle="tooltip"]').tooltip();
+//   $('[data-toggle="popover"]').popover();
+// });
 
 
 
@@ -46,19 +47,13 @@ require.context("../styles/images", true); // Подключаем папку im
 //import 'bootstrap-sass';
 
 
-
+import "../styles/application.scss";
 import './jquery.easydropdown';
-
 import './easydrop';
-
 import './jquery.flexslider';
-
 import './flexs';
-
 import './memenu';
-
 import './mem';
-
 import 'bootstrap';
 
 
